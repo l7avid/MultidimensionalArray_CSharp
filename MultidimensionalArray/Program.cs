c7f436cb02eac5d4aad2405564b6f0cf116297c0
David Reina <davidereina@hotmail.com>
2023-10-06 09:41:54 -0500

    This is a simple program that shows how to use a multi-dimensional array and how to change an specific value of it

---
+++ MultidimensionalArray/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MultidimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array2D = new string[3, 2]
+            {
+                {"one", "two"},
+                {"three", "four" },
+                {"five", "six"}
+            };
+
+            array2D[1,1] = "chicken";
+
+            Console.WriteLine(array2D[1,1]);
+        }
+    }
+}
